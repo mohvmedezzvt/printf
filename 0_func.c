0b90c78163ad29ebf4dd65cb4627ee68ec6d20b5
abdullah <abdallahfaik80@gmail.com>
2023-06-24 06:31:10 +0300

    ADD_FUNC_FILE

--- 0_func.c
+++ 0_func.c
@@ -46,21 +46,44 @@ int p_case(va_list args)
 /**
  * d_i_case - detects the formater d
  * @args: the value should be printed
- * Return: count of the number of digits
+ * Return: count of the number of digits + 1 if negative
 */
 int d_i_case(va_list args)
 {
 	int num = va_arg(args, int);
 	int count = 0;
+	int is_negative = 0;
 
 	if (num < 0)
 	{
 		write(1, "-", 1);
 		num = -num;
 		count++;
+		is_negative = 1;
 	}
-	if (num / 10)
-		count += d_i_case(num / 10);
-	count += handle_default((num % 10) + '0');
-	return (count);
+
+	// Handle the case when the number is 0 separately
+	if (num == 0)
+	{
+		count += handle_default('0');
+		return count;
+	}
+
+	// Extract the digits of the number in reverse order
+	int digits[10];
+	int num_digits = 0;
+	while (num > 0)
+	{
+		digits[num_digits] = num % 10;
+		num /= 10;
+		num_digits++;
+	}
+
+	// Print the digits in the correct order
+	for (int i = num_digits - 1; i >= 0; i--)
+	{
+		count += handle_default(digits[i] + '0');
+	}
+
+	return count + is_negative;
 }
